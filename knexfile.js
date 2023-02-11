653ffe74b9a3286d471e9d426d1533a28ee6b606
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-11 15:54:34 -0300

    update knex configs

--- knexfile.js
+++ knexfile.js
@@ -4,7 +4,14 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: path.resolve(__dirname, "src", "database", "database.db")
+    },
+    pool: {
+      afterCreate: (conn, callback) => conn.run("PRAGMA foreign_keys = ON", callback)
+    },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
+    useNullAsDefault: true
   }
 }
