1b6b09d634ea7d47b3ab5be2f6ffa2ed127105ca
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-11 16:08:30 -0300

    create knex migrations

--- src/database/knex/migrations/20230211185406_createTags.js
+++ src/database/knex/migrations/20230211185406_createTags.js
@@ -1,15 +1,8 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+exports.up = knex => knex.schema.createTable("movie_tags", table => {
+  table.increments("id");
+  table.integer("movie_id").references("id").inTable("movie_notes").onDelete("CASCADE");
+  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
+  table.text("name");
+})
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+exports.down = knex => knex.schema.dropTable("movie_tags");
