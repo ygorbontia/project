b9c3c87d97dee130eb5fe50a09bfd69807e21c92
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-12 14:44:18 -0300

    update notes routes and controller using authentication middleware

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,9 +1,10 @@
 const knex = require("../database/knex");
+const AppError = require("../utils/AppError");
 
 class NotesController {
   async create(req, res) {
     const { title, description, rating, tags } = req.body;
-    const { user_id } = req.params;
+    const user_id = req.user.id;
 
     const movie_id = await knex("movie_notes").insert({
       title,
@@ -29,14 +30,24 @@ class NotesController {
 
   async show(req, res) {
     const { id } = req.params;
+    const user_id = req.user.id;
 
     const movie = await knex("movie_notes").where({ id }).first();
+    if (movie.user_id !== user_id) {
+      throw new AppError("Você não tem permissão para acessar essa nota", 401);
+    }
 
     return res.json(movie);
   }
 
   async delete(req, res) {
     const { id } = req.params;
+    const user_id = req.user.id;
+
+    const movie = await knex("movie_notes").where({ id }).first();
+    if (movie.user_id !== user_id) {
+      throw new AppError("Você não tem permissão para excluir essa nota", 401);
+    }
 
     await knex("movie_notes").where({ id }).first().delete();
 
@@ -44,11 +55,11 @@ class NotesController {
   }
 
   async showAll(req, res) {
-    const { user_id } = req.query;
+    const user_id = req.user.id;
 
-    const notes = await knex("movie_notes").where({ user_id });
+    const notes = await knex("movie_notes").where({ user_id }).orderBy("created_at", "desc");
 
-    return res.json(notes)
+    return res.json({ notes })
   }
 }
 
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -4,7 +4,10 @@ const notesRoutes = Router();
 const NotesController = require("../controllers/NotesController");
 const notesController = new NotesController();
 
-notesRoutes.post("/:user_id", notesController.create);
+const ensureAuthentication = require("../middlewares/ensureAuthentication");
+notesRoutes.use(ensureAuthentication);
+
+notesRoutes.post("/", notesController.create);
 notesRoutes.get("/:id", notesController.show);
 notesRoutes.delete("/:id", notesController.delete);
 notesRoutes.get("/", notesController.showAll);
