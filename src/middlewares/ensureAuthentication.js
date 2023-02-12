f14df24c6a8c521a78f5636a62ab74c28b9d1e7d
Ygor Bontia <ygorbontiaa@gmail.com>
2023-02-11 23:30:28 -0300

    create authentication middleware

---
+++ src/middlewares/ensureAuthentication.js
@@ -0,0 +1,26 @@
+const AppError = require("../utils/AppError");
+const { verify } = require("jsonwebtoken");
+const authConfig = require("../configs/auth");
+
+function ensureAuthentication(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    throw new AppError("JWT Token não informado", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+  
+  try {
+    const { sub: user_id } = verify(token, authConfig.jwt.secret);
+
+    req.user = {
+      id: Number(user_id)
+    }
+
+    return next();
+  } catch {
+    throw new AppError("JWT Token inválido", 401);
+  }
+}
+
+module.exports = ensureAuthentication;
